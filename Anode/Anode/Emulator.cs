68274f96b8ae70df207aef71129f5b1e89c02d9a
Electronacl <143030637+Electronacl@users.noreply.github.com>
2026-08-10 19:03:28 +0100

    Fixed a crash to do with moving the window

--- Anode/Anode/Emulator.cs
+++ Anode/Anode/Emulator.cs
@@ -5,6 +5,8 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
 using System.Windows.Input;
 
@@ -53,8 +55,8 @@ namespace Anode
         public uint sample_rate = 44100;
         uint frame_sample_rate; // How many samples per frame
 
-        // Values which are set after the PAL/NTSC has been determined.
-
+        // Prevention of crash
+        bool can_render_frame;
 
         // ----- Unstable data
         bool changedBoundary = false;
@@ -349,6 +351,7 @@ namespace Anode
 
         // PPU output data
         public Bitmap output;
+        Bitmap op_out;
         BitmapData outputData;
         int stride;
         // Thanks to https://stackoverflow.com/questions/7768711/setpixel-is-too-slow-is-there-a-faster-way-to-draw-to-bitmap
@@ -413,6 +416,33 @@ namespace Anode
 
         // PPU Decay value
         ushort lastPPUIOUpdate;
+
+        // Clone code taken from https://stackoverflow.com/questions/78536/deep-cloning-objects/78612#78612
+        /// <summary>
+        /// Creates a deep copy of a value provided
+        /// </summary>
+        public static T Clone<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            // Don't serialize a null object, simply return the default for that object
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
 
         /// <summary>
         /// Simplified function for writing data about an opcode to the tracelog
@@ -1147,7 +1177,7 @@ namespace Anode
                     }
 
                     // Create the bitmap which will than be used in games.
-                    output = new Bitmap(32 * 8, (30 * 8) - (NTSC ? 0 : 1));
+                    op_out = new Bitmap(32 * 8, (30 * 8) - (NTSC ? 0 : 1));
 
                     // Run first init frame
                     InitFrame();
@@ -1231,7 +1261,7 @@ namespace Anode
         public void InitFrame()
         {
             // Optimisation as SetPixel is SO SLOW!
-            outputData = output.LockBits(new Rectangle(0, 0, output.Width, output.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            outputData = op_out.LockBits(new Rectangle(0, 0, op_out.Width, op_out.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
             stride = outputData.Stride;
         }
 
@@ -1364,7 +1394,8 @@ namespace Anode
         /// </summary>
         void Render()
         {
-            output.UnlockBits(outputData);
+            op_out.UnlockBits(outputData);
+            output = Clone(op_out);
             frame_Ready = true;
             // Sometimes extra code is necessary
         }
